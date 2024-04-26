9c577d737d40408c0ee18f580b3b8c96dfbb38b6
hpknollenberg <hpknollenberg@gmail.com>
2024-04-26 08:48:36 -0400

    reverted back dropdown to working state

--- src/menu.jsx
+++ src/menu.jsx
@@ -25,18 +25,18 @@ function Menu() {
               <Dropdown.Toggle className={`border-0 p-0 ps-2 pt-1 pb-1 pe-1`}
                 style={{...styles.header, ...styles.background}}>Menu</Dropdown.Toggle>
               <Dropdown.Menu className="border-0" style={{...styles.background}}>
-                <Dropdown.Item className={`ps-4`}
-                  href="/random-restaurant/" style={{...styles.header}}>Full Menu<br></br></Dropdown.Item>
-                <Dropdown.Item className={`ps-4`}
-                  href="/random-restaurant/appetizers" style={{...styles.header}}>Appetizers<br></br></Dropdown.Item>
-                <Dropdown.Item className={`ps-4`}
-                  href="/random-restaurant/dinner" style={{...styles.header}}>Dinner<br></br></Dropdown.Item>
-                <Dropdown.Item className={`ps-4`}
-                  href="/random-restaurant/lunch" style={{...styles.header}}>Lunch<br></br></Dropdown.Item>
-                <Dropdown.Item className={`ps-4`}
-                  href="/random-restaurant/breakfast" style={{...styles.header}}>Breakfast<br></br></Dropdown.Item>
-                <Dropdown.Item className={`ps-4`}
-                  href="/random-restaurant/drinks" style={{...styles.header}}>Drinks<br></br></Dropdown.Item>
+                <NavLink className={`ps-4`}
+                  to="/" style={{...styles.header}}>Full Menu<br></br></NavLink>
+                <NavLink className={`ps-4`}
+                  to="/appetizers" style={{...styles.header}}>Appetizers<br></br></NavLink>
+                <NavLink className={`ps-4`}
+                  to="/dinner" style={{...styles.header}}>Dinner<br></br></NavLink>
+                <NavLink className={`ps-4`}
+                  to="/lunch" style={{...styles.header}}>Lunch<br></br></NavLink>
+                <NavLink className={`ps-4`}
+                  to="/breakfast" style={{...styles.header}}>Breakfast<br></br></NavLink>
+                <NavLink className={`ps-4`}
+                  to="/drinks" style={{...styles.header}}>Drinks<br></br></NavLink>
               </Dropdown.Menu>
             </Dropdown>
             <NavLink className={`ps-2 pt-1 pb-1`}
